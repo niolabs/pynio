ca4450dd587202f10ea9d5df1bea83db33c2686f
Garrett Berg <gberg@neutral.io>
2015-02-11 17:29:45 -0700

    documentation"

--- pynio/service.py
+++ pynio/service.py
@@ -61,6 +61,10 @@ class Service(object):
             service.command('command')
         To a block in the service:
             service.command(block, 'command')
+
+        kwargs are passed onto the request. Some of use are:
+            data: add data onto the request
+            timeout: set the request timeout
         '''
         get = self._instance._get
         cmd_structure = '{}/{}/{}'.format
