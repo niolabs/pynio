b4ac69093a0e9104e903f537d7c4cf23643fd1e7
Garrett Berg <gberg@neutral.io>
2015-02-13 11:23:03 -0700

    minor documentation and updated the properties attributes to raise errors

--- pynio/rest.py
+++ pynio/rest.py
@@ -3,6 +3,9 @@ import json
 
 
 class REST(object):
+    '''Object for making it easier to communicate with a rest object.
+    Stores host, port and credential information and uses it automatically.
+    '''
     def __init__(self, host='127.0.0.1', port=8181, creds=None):
         self._host = host
         self._port = port
@@ -36,8 +39,7 @@ class REST(object):
 
     @host.setter
     def host(self, value):
-        print('Create a new Instance if you want a new host.')
-        pass
+        raise TypeError('Create a new Instance for a new host')
 
     @property
     def port(self):
