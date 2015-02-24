c49a13e3cd5954f6e51c4014c63c23b2ff74ba4c
Garrett Berg <gberg@neutral.io>
2015-02-24 15:06:43 -0700

    added test to find error when setting variables like 'update'

--- tests/test_properties.py
+++ tests/test_properties.py
@@ -38,6 +38,14 @@ class TestAttrDict(unittest.TestCase):
         attr.enum = 0
         assert attr.enum == 'a'
 
+    def test_set_update(self):
+        '''Makes sure that object can set values like "update" that are
+        also attributes (through item assignment)'''
+        attrdict = AttrDict(mydict)
+        attrdict['update'] = 'hi'
+        self.assertNotEqual(attrdict.update, attrdict['update'])
+        self.assertEqual(attrdict['update'], 'hi')
+
 # class TestSolid(unittest.TestCase):
 #     def test_basic(self):
 #         solid = SolidDict(mydict)
@@ -61,6 +69,19 @@ class TestTypedDict(unittest.TestCase):
         # import ipdb; ipdb.set_trace()
         convert = TypedDict(mydict)
         self.assertRaises(ValueError, setattr, convert, 'c', 'hello')
+
+    def test_set_update(self):
+        '''Makes sure that object can set values like "update" that are
+        also attributes (through item assignment)'''
+        attrdict = TypedDict(mydict)
+        attrdict['update'] = 'hi'
+        self.assertNotEqual(attrdict.update, 'hi')
+        self.assertEqual(attrdict['update'], 'hi')
+        attrdict.update({
+            'update': 'hello'
+        })
+        self.assertNotEqual(attrdict.update, 'hello')
+        self.assertEqual(attrdict['update'], 'hello')
 
     def test_readonly(self):
         frozen = TypedDict(mydict)
